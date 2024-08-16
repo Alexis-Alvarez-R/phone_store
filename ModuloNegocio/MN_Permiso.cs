8e11525bee5357448e64fc39005849fdbbb22d50
Alexis Alvarez <143042736+Alexis-Alvarez-R@users.noreply.github.com>
2024-08-16 16:43:34 -0600

    Nuevos

---
+++ ModuloNegocio/MN_Permiso.cs
@@ -0,0 +1,23 @@
+﻿using ModuloDatos;
+using ModuloEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//referencias agregadas
+using ModuloDatos;
+using ModuloEntidad;
+
+namespace ModuloNegocio
+{
+    public class MN_Permiso
+    {
+        private MD_Permiso objed_Permiso = new MD_Permiso();
+
+        public List<Permiso> Listar(int IdUsuario)
+        {
+            return objed_Permiso.Listar(IdUsuario);
+        }
+    }
+}
---
+++ ModuloNegocio/MN_Rol.cs
@@ -0,0 +1,23 @@
+﻿using ModuloDatos;
+using ModuloEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//referencias agregadas
+using ModuloDatos;
+using ModuloEntidad;
+
+namespace ModuloNegocio
+{
+    public class MN_Rol
+    {
+        private MD_Rol objed_Rol = new MD_Rol();
+
+        public List<Rol> Listar()
+        {
+            return objed_Rol.Listar();
+        }
+    }
+}
