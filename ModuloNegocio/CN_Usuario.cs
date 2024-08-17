07a49bbc227b6effbb437dc4f695ae0f8ac962e3
Alexis Alvarez <143042736+Alexis-Alvarez-R@users.noreply.github.com>
2024-08-17 13:20:52 -0600

    Update CN_Usuario.cs

--- ModuloNegocio/CN_Usuario.cs
+++ ModuloNegocio/CN_Usuario.cs
@@ -18,5 +18,67 @@ namespace ModuloNegocio
         {
             return objed_Usuario.Listar();
         }
+        
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if(obj.DocumentoUsuario == "")
+            {
+                Mensaje += "Es necesario el documento del usuario\n";
+            }
+            if(obj.NombreUsuario == "")
+            {
+                Mensaje += "Es necesario el nombre completo del usuario\n";
+            }
+            if(obj.Password == "")
+            {
+                Mensaje += "Es necesario la clave del usuario\n";
+            }
+            if(Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return Objed_Usuario.Registrar(obj, out Mensaje);
+            }
+            
+        }
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.DocumentoUsuario == "")
+            {
+                Mensaje += "Es necesario el documento del usuario\n";
+            }
+            if (obj.NombreUsuario == "")
+            {
+                Mensaje += "Es necesario el nombre completo del usuario\n";
+            }
+            if (obj.Password == "")
+            {
+                Mensaje += "Es necesario la clave del usuario\n";
+            }
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return Objed_Usuario.Editar(obj, out Mensaje);
+            }
+            
+        }
+
+        public bool Eliminar(Usuario obj, out string Mensaje)
+        {
+            return Objed_Usuario.Eliminar(obj, out Mensaje);
+        }
+    }
+}
+
     }
 }
