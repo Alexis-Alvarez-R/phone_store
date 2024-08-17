e9f18f7b75e2341934c304bcdebcfdf4787dd0e3
Alexis Alvarez <143042736+Alexis-Alvarez-R@users.noreply.github.com>
2024-08-17 16:32:38 -0600

    Nuevos

---
+++ ModuloNegocio/MN_Marca.cs
@@ -0,0 +1,63 @@
+﻿using ModuloDatos;
+using ModuloEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloNegocio
+{
+    public class MN_Marca
+    {
+        private MD_Marca Objed_Marca = new MD_Marca();
+
+        public List<Marca> Listar()
+        {
+            return Objed_Marca.Listar();
+        }
+
+        public int Registrar(Marca obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Descripcion == "")
+            {
+                Mensaje += "Es necesario la Descripcion de la Marca\n";
+            }
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return Objed_Marca.Registrar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Editar(Marca obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Descripcion == "")
+            {
+                Mensaje += "Es necesario la descripcion de la Marca\n";
+            }
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return Objed_Marca.Editar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Eliminar(Marca obj, out string Mensaje)
+        {
+            return Objed_Marca.Eliminar(obj, out Mensaje);
+        }
+    }
+}
---
+++ ModuloNegocio/MN_Producto.cs
@@ -0,0 +1,79 @@
+﻿using ModuloDatos;
+using ModuloEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloNegocio
+{
+    public class MN_Producto
+    {
+        private MD_Producto Objed_Producto = new MD_Producto();
+
+        public List<Producto> Listar()
+        {
+            return Objed_Producto.Listar();
+        }
+
+        public int Registrar(Producto obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Codigo == "")
+            {
+                Mensaje += "Es necesario el codigo del Producto\n";
+            }
+            if (obj.NombreProducto == "")
+            {
+                Mensaje += "Es necesario el nombre  del Producto\n";
+            }
+            if (obj.Descripcion == "")
+            {
+                Mensaje += "Es necesario la Descripcion  del Producto\n";
+            }
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return Objed_Producto.Registrar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Editar(Producto obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Codigo == "")
+            {
+                Mensaje += "Es necesario el codigo del Producto\n";
+            }
+            if (obj.NombreProducto == "")
+            {
+                Mensaje += "Es necesario el nombre  del Producto\n";
+            }
+            if (obj.Descripcion == "")
+            {
+                Mensaje += "Es necesario la Descripcion  del Producto\n";
+            }
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return Objed_Producto.Editar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Eliminar(Producto obj, out string Mensaje)
+        {
+            return Objed_Producto.Eliminar(obj, out Mensaje);
+        }
+    }
+}
