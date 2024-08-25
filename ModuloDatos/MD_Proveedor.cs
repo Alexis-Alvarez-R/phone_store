19f35614b5aba59a953f9609e98a8650a8241392
Alexis Alvarez <143042736+Alexis-Alvarez-R@users.noreply.github.com>
2024-08-25 17:01:48 -0600

    Nuevos

---
+++ ModuloDatos/MD_Proveedor.cs
@@ -0,0 +1,184 @@
+﻿using ModuloEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloDatos
+{
+    public class MD_Proveedor
+    {
+        //metodo que lista todos los proveedores de la BD 
+        public List<Proveedor> Listar()
+        {
+            List<Proveedor> Lista = new List<Proveedor>();
+
+            using (SqlConnection OConexion = new SqlConnection(Conexion.Cadena))
+            {
+
+                try
+                {
+                    StringBuilder Consulta = new StringBuilder();
+                    Consulta.AppendLine("select Prov_Id, Documen, Prov_Name, Gmail, Telephone, Prov_State from Proveedores");
+                    //campos de la BD
+                    SqlCommand cmd = new SqlCommand(Consulta.ToString(), OConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    OConexion.Open();
+
+                    using (SqlDataReader Lector = cmd.ExecuteReader())
+                    {
+                        while (Lector.Read())
+                        {
+
+                            Lista.Add(new Proveedor()
+                            {
+                                IdProveedor = Convert.ToInt32(Lector["Prov_Id"]),
+                                Documento = Lector["Documen"].ToString(),
+                                NombreProveedor = Lector["Prov_Name"].ToString(),
+                                Email = Lector["Gmail"].ToString(),
+                                NumeroTelefono = Lector["Telephone"].ToString(),
+                                Estado = Convert.ToBoolean(Lector["Prov_State"]),
+
+
+                            });
+
+                        }
+                    }
+
+                }
+                catch (Exception excepcion)
+                {
+                    Lista = new List<Proveedor>();
+                }
+
+            }
+
+            return Lista;
+        }
+
+        public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            int IdProveedorGenerado = 0;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection OConexion = new SqlConnection(Conexion.Cadena))
+                {
+
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarProveedor", OConexion);
+                    cmd.Parameters.AddWithValue("Documen", obj.Documento);
+                    cmd.Parameters.AddWithValue("Prov_Name", obj.NombreProveedor);
+                    cmd.Parameters.AddWithValue("Gmail", obj.Email);
+                    cmd.Parameters.AddWithValue("Telephone", obj.NumeroTelefono);
+                    cmd.Parameters.AddWithValue("Prov_State", obj.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    OConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    IdProveedorGenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception excepcion)
+            {
+                IdProveedorGenerado = 0;
+                Mensaje = excepcion.Message;
+
+
+
+            }
+
+            return IdProveedorGenerado;
+        }
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+            bool Respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection OConexion = new SqlConnection(Conexion.Cadena))
+                {
+
+                    SqlCommand cmd = new SqlCommand("sp_ModificarProveedor", OConexion);
+                    cmd.Parameters.AddWithValue("Prov_Id", obj.IdProveedor);
+                    cmd.Parameters.AddWithValue("Documen", obj.Documento);
+                    cmd.Parameters.AddWithValue("Prov_Name", obj.NombreProveedor);
+                    cmd.Parameters.AddWithValue("Gmail", obj.Email);
+                    cmd.Parameters.AddWithValue("Telephone", obj.NumeroTelefono);
+                    cmd.Parameters.AddWithValue("Prov_State", obj.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    OConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    Respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception excepcion)
+            {
+                Respuesta = false;
+                Mensaje = excepcion.Message;
+
+
+
+            }
+
+            return Respuesta;
+        }
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
+        {
+            bool Respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection OConexion = new SqlConnection(Conexion.Cadena))
+                {
+
+                    SqlCommand cmd = new SqlCommand("sp_EliminarProveedorr", OConexion);
+                    cmd.Parameters.AddWithValue("Prov_Id", obj.IdProveedor);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    OConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    Respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception excepcion)
+            {
+                Respuesta = false;
+                Mensaje = excepcion.Message;
+
+            }
+
+            return Respuesta;
+        }
+    }
+}
---
+++ ModuloDatos/MN_Proveedor.cs
@@ -0,0 +1,80 @@
+﻿using ModuloDatos;
+using ModuloEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloNegocio
+{
+    public class MN_Proveedor
+    {
+        private MD_Proveedor Objed_Proveedor = new MD_Proveedor();
+
+        public List<Proveedor> Listar()
+        {
+            return Objed_Proveedor.Listar();
+        }
+
+        public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el documento del Proveedor\n";
+            }
+            if (obj.NombreProveedor == "")
+            {
+                Mensaje += "Es necesario el nombre completo del Proveedor\n";
+            }
+            if (obj.Email == "")
+            {
+                Mensaje += "Es necesario el email del Proveedor\n";
+            }
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return Objed_Proveedor.Registrar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (obj.Documento == "")
+            {
+                Mensaje += "Es necesario el documento del Proveedor\n";
+            }
+            if (obj.NombreProveedor == "")
+            {
+                Mensaje += "Es necesario el nombre completo del Proveedor\n";
+            }
+            if (obj.Email == "")
+            {
+                Mensaje += "Es necesario el email del Proveedor\n";
+            }
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return Objed_Proveedor.Editar(obj, out Mensaje);
+            }
+
+        }
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
+        {
+            return Objed_Proveedor.Eliminar(obj, out Mensaje);
+        }
+    }
+}
+
