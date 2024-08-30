ecfa587125ca0045c2855e58f4f18725a9c793ff
Alexis Alvarez <143042736+Alexis-Alvarez-R@users.noreply.github.com>
2024-08-30 16:37:06 -0600

    Nuevo

---
+++ ModuloNegocio/MN_Compras.cs
@@ -0,0 +1,27 @@
+﻿using ModuloDatos;
+using ModuloEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloNegocio
+{
+    public class MN_Compras
+    {
+        private MD_Compras Objed_Compras = new MD_Compras();
+
+        public int ObtenerCorrelativo()
+        {
+            return Objed_Compras.ObtenerCorrelativo();
+        }
+
+        public bool Registrar(Compra obj, DataTable DetalleCompra, out string Mensaje)
+        {
+            return Objed_Compras.Registrar(obj,DetalleCompra, out Mensaje);
+        }
+
+    }
+}
