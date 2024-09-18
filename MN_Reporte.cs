fdba1aa1a1a456607f6f50b501ac55bc25566c2f
Alexis Alvarez <143042736+Alexis-Alvarez-R@users.noreply.github.com>
2024-09-18 12:45:42 -0600

    TOP FIVE

---
+++ MN_Reporte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModuloDatos;
+using ModuloEntidad;
+
+namespace ModuloNegocio
+{
+    public class MN_Reporte
+    {
+        private MD_Reporte Objed_Reporte = new MD_Reporte();
+
+        public List<ReporteCompra> Compra(string fechainicio, string fechafinal, int idproveedor)
+        {
+            return Objed_Reporte.Compra(fechainicio, fechafinal, idproveedor);
+        }
+
+        public List<ReporteVentas> Venta(string fechainicio, string fechafinal, out List<ReporteVentas> topProductos)
+        {
+          
+            return Objed_Reporte.Venta(fechainicio, fechafinal, out topProductos);
+        }
+    }
+}
